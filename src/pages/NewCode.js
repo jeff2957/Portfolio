197577e9dca143a368300d0378f6be30503aeecc
jeff2957 <jeff861208@gmail.com>
2022-12-12 23:18:27 +0800

    fix code website image link

--- src/pages/NewCode.js
+++ src/pages/NewCode.js
@@ -9,11 +9,8 @@ function NewCode() {
             <Header style={style} />
             <div className='performContainer' style={{ background: "linear-gradient(220deg, rgba(28,28,28,1) 0%, rgba(74,73,73,1) 76%, rgba(110,109,109,1) 99%, rgba(171,171,171,1) 100%)", display: "grid", overflow: "auto", overflowX: "hidden", justifyItems: "center", height: "100%" }}>
                 <h1 className='introTitle' style={{ width: "100vw", textAlign: "center", paddingTop: "11vh", paddingBottom: "20px" }}>PANGCAH Festival Website</h1>
-                {/* <iframe className='frame'
-                    // height="800vh" width="80%" 
-                    src="https://www.youtube.com/embed/vDvOkE0OGto" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowfullscreen></iframe> */}
-                    <img src='/banner.jpeg' alt='pangcah festival banner' style={{height: 'auto', width: '80vw', borderRadius: '24px'}}></img>
+                <img src='https://res.cloudinary.com/dlczspjro/image/upload/v1670855565/banner_rdjdbo.jpg' alt='pangcah festival banner' style={{height: 'auto', width: '80vw', borderRadius: '24px'}}></img>
+                <img src='https://res.cloudinary.com/dlczspjro/image/upload/v1670858104/pangcahMock_xnitm3.png' alt='pangcah festival mockup' style={{height: 'auto', width: '80vw', borderRadius: '24px', marginTop: '20px'}}></img>
                 <div style={{ marginTop: '40px', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', gap: '10px' }}>
                     <a className='btnEffct uiBtn' href='https://drive.google.com/file/d/1y5yVgU62oZIX5sMPnnSIYvt9eQtuop5Y/view?usp=share_link' target='_blank' rel="noopener noreferrer"  >View UI Guideline</a>
                     <a className='btnEffct uiBtn' href='https://drive.google.com/file/d/1PyprFwVJolI59ovd7SFBrmH9WdzZCp2L/view?usp=share_link' target='_blank' rel="noopener noreferrer">View UI Prototype</a> 
@@ -22,11 +19,11 @@ function NewCode() {
                 <div style={{ marginTop: '40px' }}>
                     <h5 className='introTitle' style={{ fontSize: "50px", width: "80vw", textAlign: "left" }}>About</h5>
                     <h6 className='descrp' style={{ width: "80vw", fontWeight: "lighter" }}>
-                        PANGCAH Festival is a music festival held in Hualien, Taiwan, October 8th- 9th, 2022. As a festival featuring mostly indigenous artists, ti advocates the concept of environmental conservation and minority justice. This project consists of two parts. One is the user interface Idesign for the festival's official website. The second part is
-the actual web development I build with Next.js and Framer Motion.
+                        PANGCAH Festival is a music festival held in Hualien, Taiwan, October 8th- 9th, 2022. As a festival featuring mostly indigenous artists, it advocates the concept of environmental conservation and minority justice. This project consists of two parts. One is the user interface design for the festival's official website. The second part is
+the actual official website I built with Next.js and Framer Motion.
                     </h6>
                 </div>
-                <div style={{ paddingBottom: "10vh", paddingTop: "40px" }}>
+                <div style={{ paddingTop: "60px" }}>
                     <h5 className='introTitle' style={{ fontSize: "50px", width: "80vw", textAlign: "left" }}>UI Design Process</h5>
                     <h6 className='descrp' style={{ width: "80vw", fontWeight: "lighter" }}>The UI design was based on the vision of the festival - living in harmony with nature
 and promoting indigenous culture. Thus, the main color used for the overall UI design is green (which represents nature) and orange (which represents the God of
@@ -37,10 +34,10 @@ web page was also created by me during the design process. Iused only texts to p
 Inside the UI prototype, several sections were made to fully exhibit the content of the festival, such as the video section, image carousel section, about section, artists
 section, events section, ticket section, and partner section. Since the background using the visual of the festival made it more difficult for users to view the text on our site, Iadded an oval-shaped block as a way to increase the clarity of the paragraph.</h6>
                 </div>
-                <div style={{ marginTop: '40px' }}>
+                <div style={{ marginTop: '60px', paddingBottom: '10vh' }}>
                     <h5 className='introTitle' style={{ fontSize: "50px", width: "80vw", textAlign: "left" }}>Development Process</h5>
                     <h6 className='descrp' style={{ width: "80vw", fontWeight: "lighter" }}>
-                    During the web development, the interface design has been constantly improved through the discussion between me and the festival organizers. The oval-shaped block was taken down to show a more consistent style throughout the whole page.
+                    During the web development, the interface design has been constantly improved through the discussion between me and the festival organizers. The oval-shaped block was taken down to present a more consistent style throughout the whole page.
 The static image background is replaced by the animation of the festival. In terms of programming language choice, I used Next.js to achieve a better
 performance compared to other Javascript frameworks. For user experience, I used Framer Motion to add transitional animations while users browsed our page.
                     </h6>
